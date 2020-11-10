381eb2873147426dec1b8f00e77ced617f3da3d2
EC2 Default User <nisi_kazu_of_world@yahoo.co.jp>
2020-11-10 04:57:14 +0000

    課題提出

---
+++ ruby03.rb
@@ -0,0 +1,40 @@
+#1
+n = 3
+if n == 3
+  p "nは3です"
+end
+
+#2
+n = 3
+if n == 3 || n == 4
+  p "3,4のどちらかです"
+end
+
+#3
+n = 3
+if n == 3
+  p "nは3です"
+elsif n == 4
+  p "nは4です"
+end
+
+#4
+for i in ["こんにちは"]*5 do
+  p i
+end
+
+#5
+[1,2,3,4,5].each do |n|
+  p n
+end
+
+#6
+n = 2
+
+if n == 2
+  p "2です"
+elsif n == 3
+  p "3です"
+else
+  p "それ以外です"
+end
